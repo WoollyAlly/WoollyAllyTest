4656ea9cd4db745ab6d62868556a8cef818ec64a
Woolly Ally <devs@woollyally.com>
2017-02-16 16:45:20 +0000

    Updated 'js/custom.js' via CloudCannon

--- js/custom.js
+++ js/custom.js
@@ -17,12 +17,6 @@ var formEl = document.getElementById("contact-form");
 formEl.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  if (grecaptcha) {
-    var recaptchaResponse = grecaptcha.getResponse();
-    if (!recaptchaResponse) { // reCAPTCHA not clicked yet
-      return false;
-    }
-  }
 
   var request = new XMLHttpRequest();
 
